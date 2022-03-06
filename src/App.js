f65475170c22067d82e73a2a4dbc00b698c64739
Axel Thiel <axth2220@gmail.com>
2022-03-06 22:56:13 +0100

    fix stuff

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import {motion, AnimatePresence} from 'framer-motion';
 import {useState} from 'react';
 
-const variants = {open: {width: 'auto', opacity: 1}, closed: {width: 0, opacity: 0}}
+const variants = {open: {width: 'auto', opacity: 1}, closed: {transition: {opacity: {duration: 0.1}}, width: 0, opacity: 0}}
 
 function App() {
   const [showField, setShowField] = useState(false);
@@ -15,13 +15,15 @@ function App() {
         <AnimatePresence>
 
         {showField &&
-        <motion.div variants={variants} initial='closed' animate='open' exit='closed' style={{display: 'flex'}} transition={{duration: 0.3}}>
-          <motion.input type="text" transition={{duration: 0.1}}/>
-          <motion.span onClick={() => setShowField(false)} transition={{duration: 0.1}}>X</motion.span>
+        <motion.div variants={variants} initial='closed' animate='open' exit='closed' style={{display: 'flex'}}>
+          <motion.input type="text" />
+          <motion.span onClick={() => setShowField(false)}>X</motion.span>
         </motion.div>
         }
         </AnimatePresence>
       </motion.div>
+      <div>hehe</div>
+      <div>heh</div>
     </div>
   );
 }
